5d2bdf0720efb1117a3c277517c4d79fd8173205
misgraal <arturpetrov07@gmail.com>
2026-03-17 21:08:00 +0200

    Update admin.py

--- app/db/admin.py
+++ app/db/admin.py
@@ -1,10 +1,9 @@
-from app.db.db import fetch_one
-from app.db.db import fetch_all
+from app.db.db import fetch_one, fetch_all
 
 async def get_user_role(user_id):
     res = await fetch_one("SELECT role FROM users WHERE userID = %s", (user_id,))
     return res["role"]
 
 async def get_users():
-    res = await fetch_all("SELECT username, role FROM users GROUP BY username")
+    res = await fetch_all("SELECT DISTINCT username, role FROM users;")
     return res
