1c0a3151e05dc64a428177804fa11fd0bcee5e18
misgraal <113765939+misgraal@users.noreply.github.com>
2026-03-17 13:45:06 +0200

    config

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.web import router
 from fastapi.staticfiles import StaticFiles
 from app.db.db import lifespan
+from config import DISKS
 
 
 app = FastAPI(lifespan=lifespan)
--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-DISCS = []
+import sys
+import os
+
+print()
+
+if sys.platform == "darwin":
+    DISKS = [f"{os.path.dirname(os.path.abspath(__file__))}/files", ]
+
+elif sys.platform.startswith("win"):
+    DISKS = [""]
